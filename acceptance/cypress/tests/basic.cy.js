4faf5c4eb9acaa343c0e6dcaafed371b3d12af5e
jonaspiterek <piterek@kitconcept.com>
2024-03-28 11:21:52 +0100

    add acceptance test

--- acceptance/cypress/tests/basic.cy.js
+++ acceptance/cypress/tests/basic.cy.js
@@ -1,8 +1,3 @@
-import {
-  getSlateEditorAndType,
-  getSelectedSlateEditor,
-} from '../support/slate';
-
 context('Basic Acceptance Tests', () => {
   describe('Text Block Tests', () => {
     beforeEach(() => {
@@ -12,9 +7,9 @@ context('Basic Acceptance Tests', () => {
       // given a logged in editor and a page in edit mode
       cy.autologin();
       cy.createContent({
-        contentType: 'Document',
-        contentId: 'document',
-        contentTitle: 'Document',
+        contentType: 'Image',
+        contentId: 'my-image',
+        contentTitle: 'My Image',
       });
       cy.visit('/');
       cy.wait('@content');
@@ -29,43 +24,20 @@ context('Basic Acceptance Tests', () => {
         .get('.documentFirstHeading')
         .contains('My Page');
 
-      getSlateEditorAndType(
-        '.block .slate-editor [contenteditable=true]',
-        'This is the text',
-      );
-
-      getSelectedSlateEditor().contains('This is the text');
-      cy.get('#toolbar-save').click();
-      cy.wait('@content');
-      cy.url().should('eq', Cypress.config().baseUrl + '/my-page');
-    });
-
-    it('As editor I can add a link to a text block', function () {
-      cy.navigate('/document/edit');
-      cy.wait('@schema');
-
-      cy.get('.block.inner.title .documentFirstHeading');
-
-      cy.log('when I create a link');
-
-      cy.getSlateEditorAndType(
-        'Colorless green ideas sleep furiously.',
-      ).setSlateSelection('furiously');
-      cy.clickSlateButton('Add link');
-      cy.get('.slate-toolbar .link-form-container input').type(
-        'https://google.com{enter}',
-      );
-
-      cy.get('#toolbar-save', { timeout: 10000 }).click();
-      cy.wait('@content');
-
-      cy.log('then the page view should contain a link');
-
-      cy.get('.container p').contains('Colorless green ideas sleep furiously.');
-      cy.get('.container p a')
-        .should('have.text', 'furiously')
-        .and('have.attr', 'href')
-        .and('include', 'https://google.com');
+      cy.get('.slate-editor > div > p').click();
+      cy.get('.text-slate-editor-inner > .ui > .icon').click();
+      cy.get('[aria-label="Unfold Common blocks"]').click();
+      cy.get(
+        '[style="transition: opacity 500ms;"] > :nth-child(10) > .ui > .icon',
+      ).click();
+      cy.get(
+        '.block > :nth-child(1) > :nth-child(1) > [tabindex="0"] > .message > center > .toolbar-inner > :nth-child(1) > .ui > .icon',
+      ).click();
+      cy.get('[title="/my-image (Image)"]').dblclick();
+      cy.get('#field-alt').type('Plone');
+      cy.get('#field-text').type('Plone 6');
+      cy.get('#field-additionalText').type('Volto Banner Block');
+      cy.get('#toolbar-save > .icon').click();
     });
   });
 });
